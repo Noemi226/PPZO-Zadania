c4c4b9a2e72bd3f83e12647c40ea54d412bfee8a
Noemi <noemi.kaczynska@student.wsb.edu.pl>
2026-05-21 22:26:19 +0200

    Prosty Kalkulator w Python

---
+++ ProstyKalkulatorPython.py
@@ -0,0 +1,28 @@
+﻿l1 = float(input("Podaj pierwszą liczbę: "))
+l2 = float(input("Podaj drugą liczbę: "))
+opcja = input("Operatory arytmetyczne \n Dodawanie + \n Odejmowanie - \n Mnożenie * \n Dzielenie / \n Wybierz operację: ")
+
+if opcja == '+':
+    wynik = l1 + l2
+
+elif opcja == '-':
+    wynik = l1 - l2
+
+elif opcja == '*':
+    wynik = l1 * l2
+
+elif opcja == '/':
+    if l2 == 0:
+        wynik = "Błąd (Nie dzieli się przez zero)"
+    else:
+        wynik = l1 / l2
+
+else:
+    wynik = "Nie rozpoznano operatora"
+
+
+#usuwanie zer po przecinku
+if isinstance(wynik, float) and wynik.is_integer():     
+    wynik = int(wynik)
+
+print(f"Wynik: {wynik}")
