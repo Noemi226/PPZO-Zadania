a9d048d8e3280f3bf8d54338e0ec6a752cbd05e0
Noemi <noemi.kaczynska@student.wsb.edu.pl>
2026-05-21 22:41:59 +0200

    Konwerter Temperatur w Python

---
+++ KonwerterTemperaturPython.py
@@ -0,0 +1,24 @@
+﻿#strip usuwa puste znaki a upper interpretuje litere z malej na duza
+wybor = input("Wybierz konwersję (C - z Celsjusza na Fahrenheita, F - z Fahrenheita na Celsjusza): ").strip().upper()       
+
+if wybor in ['C', 'F']:
+    temp = float(input("Podaj temperaturę: "))
+    
+    if wybor == 'C':
+        wynik = temp * 1.8 + 32
+        
+        if temp.is_integer(): temp = int(temp)
+        if isinstance(wynik, float) and wynik.is_integer(): wynik = int(wynik)
+            
+        print(f"{temp}°C = {wynik}°F")
+        
+    elif wybor == 'F':
+        wynik = (temp - 32) / 1.8
+        
+        if temp.is_integer(): temp = int(temp)
+        if isinstance(wynik, float) and wynik.is_integer(): wynik = int(wynik)
+            
+        print(f"{temp}°F = {wynik}°C")
+else:
+    print("Błąd! Wpisz C lub F.")
+
