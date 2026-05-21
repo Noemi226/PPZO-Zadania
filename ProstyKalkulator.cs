764575f217d61fc604c388fbc0bfce043d4dd3cd
Noemi <noemi.kaczynska@student.wsb.edu.pl>
2026-05-21 22:06:22 +0200

    Prosty kalkulator w C#

---
+++ ProstyKalkulator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Podaj pierwszą liczbę: ");
+        double l1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Podaj drugą liczbę: ");
+        double l2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Operatory arytmetyczne \n Dodawanie + \n Odejmowanie - \n Mnożenie * \n Dzielenie / \n Wybierz operację: ");
+        string opcja = Console.ReadLine();
+
+        if (opcja == "+")
+        {
+            Console.WriteLine($"Wynik: {l1 + l2}");
+        }
+        else if (opcja == "-")
+        {
+            Console.WriteLine($"Wynik: {l1 - l2}");
+        }
+        else if (opcja == "*")
+        {
+            Console.WriteLine($"Wynik: {l1 * l2}");
+        }
+        else if (opcja == "/")
+        {
+            if (l2 == 0)
+            {
+                Console.WriteLine("Wynik: Błąd (nie dzieli się przez zero)");
+            }
+            else
+            {
+                Console.WriteLine($"Wynik: {l1 / l2}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Wynik: Nie rozpoznano operatora");
+        }
+    }
+}
