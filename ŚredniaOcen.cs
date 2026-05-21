362867ed62441dbc8d02d1012ff703280c985c6f
Noemi <noemi.kaczynska@student.wsb.edu.pl>
2026-05-21 23:04:19 +0200

    Update and rename SredniaOcen.cs to ŚredniaOcen.cs

--- ŚredniaOcen.cs
+++ ŚredniaOcen.cs
@@ -11,9 +11,21 @@ class Program
 
         for (int i = 0; i < liczbaOcen; i++)
         {
-            Console.Write($"Podaj ocenę {i + 1}: ");
-            double ocena = Convert.ToDouble(Console.ReadLine());
-            sumaOcen += ocena;
+            while (true)
+            {
+                Console.Write($"Podaj ocenę {i + 1}: ");
+                double ocena = Convert.ToDouble(Console.ReadLine());
+
+                if (ocena == 1 || ocena == 2 || ocena == 3 || ocena == 4 || ocena == 5 || ocena == 6)
+                {
+                    sumaOcen += ocena;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Błąd: Dozwolone są tylko oceny 1, 2, 3, 4, 5 lub 6. Spróbuj ponownie.");
+                }
+            }
         }
 
         if (liczbaOcen > 0)
